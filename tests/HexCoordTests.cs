1d0442a5e88e70b0996c40ca5f634a9c3052728d
Taaj Street <tstr0006@student.monash.edu>
2025-11-29 21:24:46 +1100

    tests: add HexCoord tests

---
+++ tests/HexCoordTests.cs
@@ -0,0 +1,27 @@
+﻿namespace HexEngine.Tests;
+
+using HexEngine;
+
+public class HexCoordTests
+{
+    private static void AssertCubeInvariant(HexCoord h)
+    {
+        Assert.Equal(0, h.Q + h.R + h.S);
+    }
+    
+    [Property]
+    public void CubeInvariant_AlwaysHolds(int q, int r)
+    {
+        HexCoord coord = new HexCoord(q, r);
+        AssertCubeInvariant(coord);
+    }
+
+    [Property]
+    public void OffsetOddRow_RoundTrip(int q, int r)
+    {
+        HexCoord hexCoord1 = new HexCoord(q, r);
+        OffsetCoord offsetCoord = hexCoord1.ToOffsetOddRow();
+        HexCoord hexCoord2 = HexCoord.FromOffsetOddRow(offsetCoord);
+        Assert.Equal(hexCoord1, hexCoord2);
+    }
+}
